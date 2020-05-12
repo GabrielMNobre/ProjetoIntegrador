4a83d4d15fea719906b46ac481503ba7782a99b3
GabrielMNobre <gmn.nobre@gmail.com>
2020-05-12 18:15:22 -0300

    Adicionando Bem-vindo ao jogo

--- src/jogoCompleto.java
+++ src/jogoCompleto.java
@@ -1,6 +1,80 @@
+import java.util.Scanner;
 
 public class jogoCompleto {
-	public static void main(String[] args) {
 	
+	static void participacaoJogo() {
+		Scanner sc = new Scanner(System.in);
+		String confirmacao;
+		int x = 0;
+		
+		System.out.println("                                                                                                                 \n" + 
+				"88b           d88                       88        88                          88                                 \n" + 
+				"888b         d888                ,d     88        88                          88                                 \n" + 
+				"88`8b       d8'88                88     88        88                          88                                 \n" + 
+				"88 `8b     d8' 88  ,adPPYYba,  MM88MMM  88aaaaaaaa88  ,adPPYYba,   ,adPPYba,  88   ,d8    ,adPPYba,  8b,dPPYba,  \n" + 
+				"88  `8b   d8'  88  \"\"     `Y8    88     88\"\"\"\"\"\"\"\"88  \"\"     `Y8  a8\"     \"\"  88 ,a8\"    a8P_____88  88P'   \"Y8  \n" + 
+				"88   `8b d8'   88  ,adPPPPP88    88     88        88  ,adPPPPP88  8b          8888[      8PP\"\"\"\"\"\"\"  88          \n" + 
+				"88    `888'    88  88,    ,88    88,    88        88  88,    ,88  \"8a,   ,aa  88`\"Yba,   \"8b,   ,aa  88          \n" + 
+				"88     `8'     88  `\"8bbdP\"Y8    \"Y888  88        88  `\"8bbdP\"Y8   `\"Ybbd8\"'  88   `Y8a   `\"Ybbd8\"'  88          \n" + 
+				"                                                                                                              ");
+		while (x != 1) {
+			System.out.println("\n\t\t\t\t\tClique enter para jogar");
+			confirmacao = sc.nextLine();
+			
+			if (confirmacao.equals("")) {
+				x = 1;
+			} else {
+				x = 0;
+			}
+		}
+	}
+	
+	static String[] definiPersonagem() {
+		Scanner sc = new Scanner(System.in);
+		String nomePersonagem = "";
+		int idade = 0,
+			x = 0;
+		char sexo;
+		String resultado[] = new String[3];
+		
+		System.out.println("Bem-vindo ao MatHacker!");
+		System.out.println("\nVamos escolher como será seu personagem");
+		System.out.println("\nQual o sexo? \nM - Masculino \nF - Feminino");
+		sexo = sc.next().toUpperCase().charAt(0);
+		System.out.print("\nQual o nome desejado? ");
+		nomePersonagem = sc.next();
+		
+		while (x != 1) {
+			System.out.print("\nQual a idade? ");
+			idade = sc.nextInt();
+			if (idade < 18) {
+				System.out.println("O personagem precisa ser 18+ \nInsira uma idade válida!");
+				x = 0;
+			} else {
+				x = 1;
+			}
+		}
+		
+		resultado[0] = String.valueOf(sexo); 
+		resultado[1] = nomePersonagem;
+		resultado[2] = String.valueOf(idade);
+		
+		return resultado;
+	}
+	
+	public static void main(String[] args) {
+		int idadePersonagem = 0;
+		char sexoPersonagem;
+		String nomePersonagem = "";
+		String informacoesPersonagem[] = new String[3];
+		
+		participacaoJogo();
+		informacoesPersonagem = definiPersonagem();
+		
+		sexoPersonagem = informacoesPersonagem[0].charAt(0);
+		nomePersonagem = informacoesPersonagem[1];
+		idadePersonagem = Integer.parseInt(informacoesPersonagem[2]);
+		
+		System.out.println("\nSexo => "+sexoPersonagem+"\nNome => "+nomePersonagem+"\nIdade => "+idadePersonagem);
 	}
 }
